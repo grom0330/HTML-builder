4dbf8475beb279d7ca53af76425f2fbc51f25715
grom0330 <tokarev-oleg2004@mail.ru>
2022-11-08 20:19:22 +0300

    feat: complete 02 and 03

---
+++ 02-write-file/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('node:readline');
+const process = require('process');
+
+const path_join = path.join(__dirname, 'text.txt');
+
+fs.open(path_join, 'a+', (error) => {
+    if(error) throw error;
+
+    console.log("What do you want to note ?:");
+})
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+rl.on('line', (info) => {
+  info.trim().toLowerCase() === 'exit' ? rl.close() : 
+  fs.appendFile(path_join, `${info}\n`, 
+  (error) => {if(error) throw error});
+})
+
+process.on('SIGINT', () => {
+    console.log('Buy)');
+    rl.close();
+});
---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+
+const path_join = path.join(__dirname, "secret-folder");
+
+fs.readdir(path_join, { withFileTypes: true }, (error, files) => {
+  if (error) throw error;
+
+  files.forEach((file) => {
+    if (file.isDirectory() == false) {
+      fs.stat(path.join(path_join, file.name), (error, stats) => {
+        if (error) throw error;
+
+        console.log(
+          path.basename(file.name, path.extname(file.name)),
+          "-",
+          path.extname(file.name).slice(1),
+          "-",
+          stats["size"],
+          "bytes"
+        );
+      });
+    }
+  });
+});
